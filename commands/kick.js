b8c0183337c1a90da734b6c2f92ca353c1fb9b77
sp0kl <mccabe1204@gmail.com>
2018-08-16 13:55:34 -0400

    updated kick command

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
-const discord = require("discord.js");
-let perms = message.member.permissions;
-let hasPerms = message.member.hasPermissions("KICK_MEMBERS");
-let user = message.mentions.members.first();
-let reason = args.slicer(1).join(" ");
-if (hasPerms) {
+  const discord = require("discord.js");
+  let perms = message.member.permissions;
+  let hasPerms = message.member.hasPermissions("KICK_MEMBERS");
+  let user = message.mentions.members.first();
+  let reason = args.slice(1).join(" ");
+  if (hasPerms) {
     user.kick(reason);
     message.channel.send(`${user} has been kicked for ${reason}!`).catch(err => {
       message.channel.send(`I was unable to kick ${user}`);
@@ -14,4 +14,3 @@ if (hasPerms) {
     message.channel.send(`You do not have permission to kick ${user}!`);
   }
 };
-}
